d74247ee12e8d93c9d422536da0098d80977421b
Serghei Amelian <serghei.amelian@gmail.com>
2015-11-04 16:05:25 +0200

    fixed FTBS for newer perl

--- dcop/dcopidlng/kdocAstUtil.pm
+++ dcop/dcopidlng/kdocAstUtil.pm
@@ -438,7 +438,7 @@ sub dumpAst
 		dumpAst( $kid );
 	}
 
-	print "\t" x $depth, "Documentation nodes:\n" if defined 
+	print "\t" x $depth, "Documentation nodes:\n" if
 		@{ $node->{Doc}->{ "Text" }};
 
 	foreach $kid ( @{ $node->{Doc}->{ "Text" }} ) {
