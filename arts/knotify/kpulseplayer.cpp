67359e090d104299e89d7262d7e6a4d1e7198038
Serghei Amelian <serghei.amelian@gmail.com>
2016-02-21 00:58:49 +0200

    added wav support to KPulsePlayer

--- arts/knotify/kpulseplayer.cpp
+++ arts/knotify/kpulseplayer.cpp
@@ -19,6 +19,7 @@
 //#include <atomic>
 #include <pulse/simple.h>
 #include <pulse/error.h>
+#include <audiofile.h>
 #include <vorbis/codec.h>
 #include <vorbis/vorbisfile.h>
 #include <qapplication.h>
@@ -43,6 +44,7 @@ protected:
 
 private:
 	void runOgg();
+	void runWav();
 
 private:
 	KPulsePlayerPrivate *parent;
@@ -70,7 +72,14 @@ KPulsePlayerWorker::~KPulsePlayerWorker()
 
 void KPulsePlayerWorker::run()
 {
-	runOgg();
+	QCString endsWith = file.right(4);
+
+	if(".ogg" == endsWith)
+		runOgg();
+	else if(".wav" == endsWith)
+		runWav();
+	else
+		kdDebug() << "KPulsePlayerWorker::run(): unsupported format \"" << endsWith << "\"" << endl;
 
 	if(!do_cancel)
 		QApplication::postEvent(reinterpret_cast<QObject*>(parent), new QCustomEvent(QEvent::User, file.data()));
@@ -158,6 +167,79 @@ void KPulsePlayerWorker::runOgg()
 
 VORBIS_CLEANUP:
 	ov_clear(&vf);
+}
+
+
+void KPulsePlayerWorker::runWav()
+{
+	AFfilehandle af = afOpenFile(file.data(), "r", AF_NULL_FILESETUP);
+
+	if(!af)
+	{
+		kdDebug() << "KPulsePlayerWorker::runWav() - could not open " << file << endl;
+		return;
+	}
+
+	afSetVirtualByteOrder(af, AF_DEFAULT_TRACK, AF_BYTEORDER_LITTLEENDIAN);
+
+	int sampleFormat, sampleWidth;
+	afGetVirtualSampleFormat(af, AF_DEFAULT_TRACK, &sampleFormat, &sampleWidth);
+
+	// KPulsePlayer only supports 8 bit unsigned and 16 bit signed data
+	// so force it to be compatible.
+	if(8 == sampleWidth)
+		afSetVirtualSampleFormat(af, AF_DEFAULT_TRACK, AF_SAMPFMT_UNSIGNED, 8);
+	else
+		afSetVirtualSampleFormat(af, AF_DEFAULT_TRACK, AF_SAMPFMT_TWOSCOMP, 16);
+
+	pa_sample_spec ss;
+	ss.format = (8 == sampleWidth ? PA_SAMPLE_U8 : PA_SAMPLE_S16LE);
+	ss.rate = afGetRate(af, AF_DEFAULT_TRACK);
+	ss.channels = afGetChannels(af, AF_DEFAULT_TRACK);
+
+	int sampleSize = ss.channels * (sampleWidth / 8);
+
+	int error;
+	pa_simple *s = pa_simple_new(NULL, "KNotify", PA_STREAM_PLAYBACK, NULL, "Playback", &ss, NULL, NULL, &error);
+
+	if(NULL == s)
+	{
+		kdDebug() << "KPulsePlayerWorker::runWav() - pulseaudio: " << pa_strerror(error) << endl;
+		goto AUDIOFILE_CLEANUP;
+	}
+
+	for(;!do_cancel;)
+	{
+		if(do_rewind)
+		{
+			afSeekFrame(af, AF_DEFAULT_TRACK, 0);
+			do_rewind = false;
+		}
+
+		char buf[4192 * sampleSize];
+		AFframecount len = afReadFrames(af, AF_DEFAULT_TRACK, buf, sizeof(buf) / sampleSize);
+
+		if(0 < len)
+		{
+			if(0 > pa_simple_write(s, buf, len * sampleSize, &error))
+			{
+				kdDebug() << "KPulsePlayerWorker::runWav() - pulseaudio: " << pa_strerror(error) << endl;
+				break;
+			}
+		}
+		else
+		{
+			if(0 > len)
+				kdDebug() << "KPulsePlayerWorker::runWav() - audiofile: the wav stream seems corrupted, abort playing" << endl;
+			break;
+		}
+	}
+
+	pa_simple_drain(s, &error);
+	pa_simple_free(s);
+
+AUDIOFILE_CLEANUP:
+	afCloseFile(af);
 }
 
 
